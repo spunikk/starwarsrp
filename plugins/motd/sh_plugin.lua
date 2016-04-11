ef5cd83e2b40e71da3074c1e6b6acbf6aa97ad3e
dasinternetreich <dasinternetreich@gmail.com>
2016-04-11 09:22:24 -0400

    Added files via upload

---
+++ plugins/motd/sh_plugin.lua
@@ -0,0 +1,6 @@
+PLUGIN.name = "MOTD"
+PLUGIN.author = "Black Tea (NS 1.0), Neon (NS 1.1)"
+PLUGIN.desc = "Allows you read motds."
+PLUGIN.url = "http://www.formworkdepot.com.au/wp-content/uploads/2015/11/underconstruction.jpg" --< Change URL here
+
+nut.util.include("sh_menu.lua")
