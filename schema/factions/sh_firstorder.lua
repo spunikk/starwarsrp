8c0f81227e6b89434efd460418f5b58d84fe4f70
dasinternetreich <dasinternetreich@gmail.com>
2016-04-11 02:59:17 -0400

    Update sh_firstorder.lua

--- schema/factions/sh_firstorder.lua
+++ schema/factions/sh_firstorder.lua
@@ -2,3 +2,11 @@ FACTION.name = "First Order"
 FACTION.isdefault = "false"
 FACTION.desc = "None"
 FACTION.color = Color(225, 0, 0)
+
+FACTION.isDefault = false
+
+FACTION.pay = 100
+FACTION.payTime = 600
+FACTION.isGloballyRecognized = true
+
+FACTION_FO = FACTION.index
