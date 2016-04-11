7b819dd8784a64372e317e981e08309cd32dd098
dasinternetreich <dasinternetreich@gmail.com>
2016-04-11 02:59:41 -0400

    Update sh_firstorderrecruites.lua

--- schema/factions/sh_firstorderrecruites.lua
+++ schema/factions/sh_firstorderrecruites.lua
@@ -2,3 +2,9 @@ FACTION.name = "First Order Recruits"
 FACTION.isdefault = "true"
 FACTION.desc = "None"
 FACTION.color = Color(0, 0, 0)
+
+FACTION.pay = 100
+FACTION.payTime = 600
+FACTION.isGloballyRecognized = true
+
+FACTION_FO = FACTION.index
