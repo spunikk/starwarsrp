f64c52d124af75604078a5d26168341567032590
dasinternetreich <dasinternetreich@gmail.com>
2016-04-11 03:00:20 -0400

    Update sh_rebelalliance.lua

--- schema/factions/sh_rebelalliance.lua
+++ schema/factions/sh_rebelalliance.lua
@@ -2,3 +2,8 @@ FACTION.name = "Rebel Alliance"
 FACTION.isdefault = "false"
 FACTION.desc = "None"
 FACTION.color = Color(255, 100, 0)
+FACTION.pay = 100
+FACTION.payTime = 600
+FACTION.isGloballyRecognized = true
+
+FACTION_FO = FACTION.index
