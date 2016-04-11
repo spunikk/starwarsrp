680697f9944c3fc17730ea2ff78615a6c5c377b1
dasinternetreich <dasinternetreich@gmail.com>
2016-04-11 09:24:28 -0400

    Added files via upload

---
+++ plugins/business/items/sh_permit_admin.lua
@@ -0,0 +1,5 @@
+ITEM.name = "Admin Items"
+ITEM.desc = "Only for Admins."
+ITEM.price = 999999
+ITEM.model = "models/props_lab/clipboard.mdl"
+ITEM.category = "Permits"
---
+++ plugins/business/items/sh_permit_medic.lua
@@ -0,0 +1,5 @@
+ITEM.name = "Medical License"
+ITEM.desc = "A piece of paper that that certifies your medical knowledge."
+ITEM.price = 900
+ITEM.model = "models/props_lab/clipboard.mdl"
+ITEM.category = "Permits"
