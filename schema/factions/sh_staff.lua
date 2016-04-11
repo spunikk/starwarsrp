c702090df459e17875a31d211593e674aca5cf95
dasinternetreich <dasinternetreich@gmail.com>
2016-04-11 09:26:13 -0400

    Added files via upload

---
+++ schema/factions/sh_staff.lua
@@ -0,0 +1,13 @@
+FACTION.name = "Staff"
+FACTION.desc = "Do not RP"
+FACTION.color = Color(10, 10, 10, 255)
+FACTION.isDefault = false
+FACTION.models = {
+	"models/kriegsyntax/germans/ss/lss/enlisted/schutze/npc.mdl"
+}
+
+FACTION.pay = 0
+FACTION.payTime = 600
+FACTION.isGloballyRecognized = true
+
+FACTION_HEER = FACTION.index
